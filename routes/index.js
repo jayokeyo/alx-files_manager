c4348b7afc1d732a4fc68236487971c2a5f7386a
samuelsenerwa <samuelsenerwa@gmail.com>
2023-10-02 12:12:23 +0300

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,11 @@
 import { Router } from "express";
+import AppController from "../controllers/AppController";
+
+// contains all the endpoints for our APIs
+
+const router = Router();
+
+router.get("/status", AppController.getStatus);
+router.get("/stats", AppController.getStats);
+
+module.exports = router;
