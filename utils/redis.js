52cbf7476f8ab4420bf5757efa21d10f21fee234
samuelsenerwa <samuelsenerwa@gmail.com>
2023-10-02 11:12:26 +0300

    task files

---
+++ utils/redis.js
@@ -0,0 +1,45 @@
+import { RedisClient } from "redis";
+
+// Class RedisClient containing a constructor
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on("error", (error) => {
+      console.log(`Redis client not connected to server: ${error}`);
+    });
+  }
+
+  //   Check if there is connection error with redis
+
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  //   an asynchronous function get that takes a string key as argument and returns the Redis value stored for this key
+  async get(key) {
+    const redisGet = promisfy(this.client.get).bind(this.client);
+    const value = await redisGet(key);
+    return value;
+  }
+
+  //   Set key vlue pair to redis server
+  async set(key, value, time) {
+    const redisSet = promisfy(this.client.set).bind(this.client);
+    await redisSet(key, value);
+    await this.client.expire(key, time);
+  }
+
+  //   an asynchronous function del that takes a string key as argument and remove the value in Redis for this key
+  async del(key) {
+    const redisDel = promisfy(this.client.del).bind(this.client);
+    await redisDel(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
