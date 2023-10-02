2b21740af5b6791c6b8e783fae92ee38e938028b
muganedj <muganes25@gmail.com>
2023-10-02 16:33:47 +0300

    0x3

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 
 import { Router } from "express";
 import AppController from "../controllers/AppController";
-import UserController from "../controllers/UserController";
+import UsersController from "../controllers/UsersController";
 
 // contains all the endpoints for our APIs
 
@@ -10,6 +10,6 @@ const router = Router();
 
 router.get("/status", AppController.getStatus);
 router.get("/stats", AppController.getStats);
-router.post("/users", UserController.postNew);
+router.post("/users", UsersController.postNew);
 
 module.exports = router;
